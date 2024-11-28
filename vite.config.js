d75429b8f2a9233b5e3bc6f7e5f9311b64591b57
신혜민 <shinhyemin225@gmail.com>
2024-11-28 16:46:16 +0900

    Fix build and deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
+export default {
+  base: './',
   build: {
-    outDir: 'dist', // 빌드 결과물이 저장될 디렉토리
+    outDir: 'dist',
   },
-});
+};
+
