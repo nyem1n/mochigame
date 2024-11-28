5e0f28565f80ed17a4f3dbcf19e17c88eede60d7
신혜민 <shinhyemin225@gmail.com>
2024-11-28 17:27:36 +0900

    Update Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
-  base: './',
+export default defineConfig({
+  plugins: [react()],
+  base: './', // 배포 경로를 현재 경로에 맞추기
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // 빌드 출력 디렉토리
+    assetsDir: 'assets', // 정적 파일 디렉토리 설정
   },
-};
-
+});
