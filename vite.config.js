0fcc1f6859efe1ba2bde6cab50808e8a8e80109a
신혜민 <shinhyemin225@gmail.com>
2024-11-28 20:43:01 +0900

    test2

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 배포 경로를 현재 경로에 맞추기
+  base: './', // 로컬 및 배포 환경 모두에서 올바르게 동작
   build: {
-    outDir: 'dist', // 빌드 출력 디렉토리
-    assetsDir: 'assets', // 정적 파일 디렉토리 설정
-    publicDir: 'src/assets',
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
